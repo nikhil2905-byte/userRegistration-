3ccd855e3f0da6807b645ee8a7cf8179cf4e4440
unknown <nikhilnakirikanti27@gmail.com>
2020-10-27 11:50:41 +0530

    add : First Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
 echo "Welcome to User Registration Problem"
+validateFirstName(){
+read -p "Enter Name:" Name
+NamePattern="^[A-Z][a-zA-Z]{2,}$"
+    if [[ $Name =~ $NamePattern ]];
+    then
+        echo "First Name is valid"
+    else
+        echo "First Name is invalid"
+    fi
+}
+
+validateFirstName $Name
+
+
